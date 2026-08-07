7ebf52746c929e973ef5add4983b0ed97657385c
nataraz <natarazjavaarena@gmail.com>
2026-08-07 09:38:29 +0530

    task1  by  dev1

--- MavenProj01/src/main/java/com/nt/main/App.java
+++ MavenProj01/src/main/java/com/nt/main/App.java
@@ -1,10 +1,14 @@
 package com.nt.main;
 
+import com.nt.service.ArithmeticService;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!-1231");
+        ArithmeticService  service=new ArithmeticService();
+        System.out.println("result is ::"+service.sum(100, 200));
     }
 }
---
+++ MavenProj01/src/main/java/com/nt/service/ArithmeticService.java
@@ -0,0 +1,10 @@
+//ArithmeticService.java
+package com.nt.service;
+
+public class ArithmeticService {
+	
+	public  int sum(int x,int y) {
+		return x+y;
+	}
+
+}
