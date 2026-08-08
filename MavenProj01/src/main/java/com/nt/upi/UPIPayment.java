578e1ffd0d4eb4573ce10e6c9afa11e89e1f409b
nataraz <natarazjavaarena@gmail.com>
2026-08-08 09:39:27 +0530

    TL-UPI Payment branch updation

--- MavenProj01/src/main/java/com/nt/upi/UPIPayment.java
+++ MavenProj01/src/main/java/com/nt/upi/UPIPayment.java
@@ -5,7 +5,8 @@ public class UPIPayment {
 	public   String  doPayment(long phoneNo, double amount) {
 		
 		System.out.println("UPIPayment.doPayment()  ");
-		
+		 if(phoneNo<0  ||  amount<0)
+			 throw new  IllegalArgumentException("Invalid Id");
 		return  amount+"  amount  is  transfered to::"+phoneNo;
 		
 	}
