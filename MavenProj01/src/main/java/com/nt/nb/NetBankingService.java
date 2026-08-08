f0cd8c253273ce5bf10f83d599f48b8b7609d5be
nataraz <natarazjavaarena@gmail.com>
2026-08-08 10:17:47 +0530

    TL- ccheque payment is added

--- MavenProj01/src/main/java/com/nt/nb/NetBankingService.java
+++ MavenProj01/src/main/java/com/nt/nb/NetBankingService.java
@@ -10,4 +10,12 @@ public class NetBankingService {
 		
 		return amount+" amount is transfered from "+srcAcno+" to"+destAcno;
 	}
+	
+	public  String  doChequePayment(long chequeNo,  double amount) {
+		System.out.println("NetBankingService.doChequePayment()");
+		if(chequeNo<0 ||  amount<0)
+			throw  new IllegalArgumentException("Invalid inputs");
+		
+		return  amount+" amount is  withdrawn  using cheque Number ::"+chequeNo;
+	}
 }
