cd1b18cdbbdacc61953e281af34efb01b9d999ca
nataraz <natarazjavaarena@gmail.com>
2026-08-08 09:52:40 +0530

    TL- NetBanking Branch Updation

--- MavenProj01/src/main/java/com/nt/nb/NetBankingService.java
+++ MavenProj01/src/main/java/com/nt/nb/NetBankingService.java
@@ -5,6 +5,9 @@ public class NetBankingService {
 	
 	public  String    doPayment(long srcAcno,long destAcno, double  amount) {
 		System.out.println("NetBankingService.doPayment()");
+		if(srcAcno<0  ||  destAcno<0  ||  amount<0)
+			throw  new IllegalArgumentException("Invalid  inputs");
+		
 		return amount+" amount is transfered from "+srcAcno+" to"+destAcno;
 	}
 }
