4a2f24df8c778a066ea00fbdd94edcd28766900c
nataraz <natarazjavaarena@gmail.com>
2026-08-08 10:39:48 +0530

    chanage1

--- MavenProj03/src/main/java/com/nt/service/Arithmetic.java
+++ MavenProj03/src/main/java/com/nt/service/Arithmetic.java
@@ -4,10 +4,12 @@ package com.nt.service;
 public class Arithmetic {
 
 	public  int sum(int x,int y) {
+		System.out.println("Arithmetic.sum()");
 		return x+y;
 	}
 	
 	public  int sub(int x, int y) {
+		System.out.println("Arithmetic.sub()");
 		return x-y;
 	}
 }
